8792a47d8281ae9480e3a42f452b7f7924b7d3a2
TemurMannonov <t.mannonov@gmail.com>
2020-04-17 22:55:32 +0500

    courier vehicles and details crud

--- migrations/1_create_tables.up.sql
+++ migrations/1_create_tables.up.sql
@@ -6,15 +6,9 @@ CREATE TABLE IF NOT EXISTS distributor (
      status BOOLEAN NOT NULL DEFAULT TRUE
 );
 
-CREATE TABLE IF NOT EXISTS distributor_fare (
-     distributor_id uuid REFERENCES distributor(id) ON DELETE CASCADE,
-     fare_id uuid,
-     PRIMARY KEY(distributor_id, fare_id)
-);
-
 CREATE TABLE IF NOT EXISTS courier (
-     id uuid PRIMARY KEY,distributor_id uuid R
-     distributor_id uuid REFERENCES distributor(id) NOT NULL,
+     id uuid PRIMARY KEY,
+     distributor_id uuid REFERENCES distributor(id) ON DELETE CASCADE,
      phone VARCHAR(100) NOT NULL,
      first_name VARCHAR(100) NOT NULL,
      last_name VARCHAR(100) NOT NULL,
@@ -26,18 +20,12 @@ CREATE TABLE IF NOT EXISTS courier_details (
      courier_id uuid PRIMARY KEY REFERENCES courier(id) ON DELETE CASCADE,
      passport_number VARCHAR(100) NOT NULL,
      gender VARCHAR(100),
-     birth_date VARCHAR(100) NOT NULL,
+     birth_date TIMESTAMP NOT NULL,
      address VARCHAR(100),
      img VARCHAR(100),
      lisense_number VARCHAR(100) NOT NULL,
      lisense_given_date TIMESTAMP NOT NULL,
      lisense_expiry_date TIMESTAMP NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS courier_fare (
-     courier_id uuid REFERENCES courier(id) ON DELETE CASCADE,
-     fare_id uuid,
-     PRIMARY KEY(courier_id, fare_id)
 );
 
 CREATE TABLE IF NOT EXISTS courier_vehicle (
